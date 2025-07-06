d20ba5ad1bad3a25b41fe81ba0233542900f6698
AidanJacoby <aidanjacoby247@gmail.com>
2025-07-06 15:13:43 -0700

    Added Python Project and Word Document

---
+++ module-9/module-9.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+#response = requests.get("http://api.open-notify.org/astros.json")
+#print(response.status_code)
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=2)
+    print(text)
+    
+#jprint(response.json())
+
+
+chosenClass = requests.get("https://www.dnd5eapi.co/api/2014/classes")
+print(chosenClass.status_code)
+def main():
+    try:
+        myClass = input("Please Select Your Class:")
+        apiString = "https://www.dnd5eapi.co/api/2014/classes/" + myClass
+        chosenClass = requests.get(apiString)
+        print(chosenClass.status_code)
+        jprint(chosenClass.json())
+    except:
+        print("Invalid argument, Please Try again.")
+        print(chosenClass.status_code)
+        main()
+        
+main()
